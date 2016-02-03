317b914cb5f6a07a9fcf87eb090e6a4e956db28d
Syeponde <Syeponde@23098e8b-90bb-4560-97df-f042a60ff307>
2016-02-03 13:58:05 +0000

    Point de stabilisation 2.1 (création du menu de choix pile ou face)
    git-svn-id: svn://localhost/pong@43 23098e8b-90bb-4560-97df-f042a60ff307

--- PongSlick2D/src/etatsJeu/EtatMiseEnJeu.java
+++ PongSlick2D/src/etatsJeu/EtatMiseEnJeu.java
@@ -3,6 +3,7 @@ package etatsJeu;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Polygon;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -15,13 +16,16 @@ public class EtatMiseEnJeu extends BasicGameState
 	public static final int ID = ConstantesEtat.ETAT_MISE_EN_JEU;
 	private StateBasedGame jeu;
 	private Rectangle r;
+	private Polygon p;
 
 	@Override
 	public void init(GameContainer gameContainer, StateBasedGame jeu) throws SlickException 
 	{
 		// TODO Auto-generated method stub
 		this.setJeu(jeu);
-		r = new Rectangle((gameContainer.getWidth()/2) - 150, (gameContainer.getHeight()/2) - 150, 300, 300);
+		r = new Rectangle((gameContainer.getWidth()/2) - 150, (gameContainer.getHeight()/2) - 40, 300, 100);
+		p = new Polygon(new float[]{100f,100f,150f,100f,32f,32f,6f,32f});
+		
 		
 	}
 
@@ -29,6 +33,9 @@ public class EtatMiseEnJeu extends BasicGameState
 	public void render(GameContainer gameContainer, StateBasedGame jeu, Graphics graphisme) throws SlickException 
 	{
 		graphisme.draw(r);
+		graphisme.drawString("Veuillez choisir!", r.getX() + 80, r.getY() + 20);
+		graphisme.drawString("PILE", r.getX() + 130, r.getY() + 40);
+		graphisme.drawString("FACE", r.getX() + 130, r.getY() + 60);
 		// TODO Auto-generated method stub
 	}
 
